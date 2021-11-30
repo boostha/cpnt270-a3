32d224121989d550da65e1345aa6e298b1cd2589
David Ha <David.ha0908@gmail.com>
2021-11-29 19:33:04 -0700

    tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,11 @@ const init = async () => {
   const response = await fetch('/.netlify/functions/event');
   const data = await response.json();
 
-  const newsFigure = data.find((item) => item.caption.includes('#news'));
+  const newsFigure = await data.find((item) => item.caption.includes('#news'));
 
-  // websiteIndex = await newsFigure.search('http')
-  // address = await newsFigure.slice(websiteIndex)
-  // cap = await newsFigure.slice(0, websiteIndex)
+  websiteIndex = newsFigure.search('http')
+  address = newsFigure.slice(websiteIndex)
+  cap = newsFigure.slice(0, websiteIndex)
 
   document.querySelector('.news').innerHTML = `
   <figure>
