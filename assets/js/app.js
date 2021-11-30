c931781408ecd90a3058885a00d45d3bb8d3439b
David Ha <David.ha0908@gmail.com>
2021-11-29 19:23:20 -0700

    tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,15 @@ let websiteIndex
 let address
 let cap
 
-const init = () => {
-  const response = fetch('/.netlify/functions/event');
-  const data = response.json();
+const init = async () => {
+  const response = await fetch('/.netlify/functions/event');
+  const data = await response.json();
 
   const newsFigure = data.find((item) => item.caption.includes('#news'));
 
-  websiteIndex = newsFigure.search('http')
-  address = newsFigure.slice(websiteIndex)
-  cap = newsFigure.slice(0, websiteIndex)
+  // websiteIndex = await newsFigure.search('http')
+  // address = await newsFigure.slice(websiteIndex)
+  // cap = await newsFigure.slice(0, websiteIndex)
 
   document.querySelector('.news').innerHTML = `
   <figure>
