7adb7afc1c8c4598572a538540033c4aaf9db6d4
David Ha <David.ha0908@gmail.com>
2021-11-29 19:43:42 -0700

    tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
 let websiteIndex
 let address
 let cap
+let hashIndex
 
-const init = async () => {
+const newsFun = async () => {
   const response = await fetch('/.netlify/functions/event');
   const data = await response.json();
 
@@ -10,13 +11,14 @@ const init = async () => {
   console.log(newsFigure)
 
   websiteIndex = newsFigure.caption.search('http')
+  hashIndex = newsFigure.caption.search('#')
   address = newsFigure.caption.slice(websiteIndex)
-  cap = newsFigure.caption.slice(0, websiteIndex)
+  cap = newsFigure.caption.slice(0, hashIndex)
 
   document.querySelector('.news').innerHTML = `
   <figure>
     <a href="${address}">
-      <img src="${newsFigure.url}" alt="">
+      <img src="${newsFigure.url}" alt="" width="500">
     </a>
     <figcaption>${cap}</figcaption>
   </figure>
@@ -25,4 +27,54 @@ const init = async () => {
   console.log(data);
 }
 
-init();
+const foodFun = async () => {
+  const response = await fetch('/.netlify/functions/event');
+  const data = await response.json();
+
+  const foodFigure = await data.find((item) => item.caption.includes('#food'));
+
+  websiteIndex = foodFigure.caption.search('http')
+  hashIndex = foodFigure.caption.search('#')
+  address = foodFigure.caption.slice(websiteIndex)
+  cap = foodFigure.caption.slice(0, hashIndex)
+
+  document.querySelector('.news').innerHTML = `
+  <figure>
+    <a href="${address}">
+      <img src="${foodFigure.url}" alt="" width="500">
+    </a>
+    <a href="${address}">
+    <figcaption>${cap}</figcaption>
+    </a>
+  </figure>
+  `
+
+  console.log(data);
+}
+
+const blogFun = async () => {
+  const response = await fetch('/.netlify/functions/event');
+  const data = await response.json();
+
+  const blogFigure = await data.find((item) => item.caption.includes('#blog'));
+
+  websiteIndex = blogFigure.caption.search('http')
+  hashIndex = blogFigure.caption.search('#')
+  address = blogFigure.caption.slice(websiteIndex)
+  cap = blogFigure.caption.slice(0, hashIndex)
+
+  document.querySelector('.news').innerHTML = `
+  <figure>
+    <a href="${address}">
+      <img src="${blogFigure.url}" alt="" width="500">
+    </a>
+    <figcaption>${cap}</figcaption>
+  </figure>
+  `
+
+  console.log(data);
+}
+
+newsFun();
+foodFun();
+blogFun();
