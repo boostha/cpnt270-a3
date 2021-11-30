1393c5e18c19a849c77f5ede29e5c4a916d68c53
David Ha <David.ha0908@gmail.com>
2021-11-29 19:18:01 -0700

    tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,25 @@
 let websiteIndex
 let address
 let cap
-let output = ''
 
 const init = async () => {
   const response = await fetch('/.netlify/functions/event');
   const data = await response.json();
 
   const newsFigure = data.find((item) => item.caption.includes('#news'));
-  
-  newsFigure.forEach((picture) => {
 
+  websiteIndex = newsFigure.search('http')
+  address = newsFigure.slice(websiteIndex)
+  cap = newsFigure.slice(0, websiteIndex)
 
-    websiteIndex = newsFigure.search('http')
-    address = newsFigure.slice(websiteIndex)
-    cap = newsFigure.slice(0, websiteIndex)
-    
-    output += 
-    `
-    <figure>
-      <a href="${address}">
-        <img src="${picture.url}" alt="" width="500" height="500">
-      </a>
-      <figcaption>
-        <p>${cap}</p>
-      </figcaption>
-    </figure>
-    `
-  })
+  document.querySelector('.news').innerHTML = `
+  <figure>
+    <img src="${newsFigure.url}" alt="">
+    <figcaption>${cap}</figcaption>
+  </figure>
+  `
 
   console.log(data);
 }
 
 init();
-document.querySelector('.news').innerHTML = output
